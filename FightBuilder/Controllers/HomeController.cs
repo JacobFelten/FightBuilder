aecef9084997bd5791bf20ff53f4b992fc5f6421
SherlockJake <7thejake7@gmail.com>
2019-11-04 20:03:57 -0800

    Added views, _Layout, and home controller

---
+++ FightBuilder/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FightBuilder.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DesignEquipment()
+        {
+            return View();
+        }
+
+        public IActionResult BuildAFighter()
+        {
+            return View();
+        }
+
+        public IActionResult Fight()
+        {
+            return View();
+        }
+
+        public IActionResult DataTables()
+        {
+            return View();
+        }
+    }
+}
