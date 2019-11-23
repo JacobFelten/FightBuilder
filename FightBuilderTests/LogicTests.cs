385703830213fbd17b7bc73138f562b58abe04b9
SherlockJake <7thejake7@gmail.com>
2019-11-23 14:41:38 -0800

    Added unit test project

---
+++ FightBuilderTests/LogicTests.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+using FightBuilder.Other;
+using FightBuilder.Models;
+
+namespace FightBuilderTests
+{
+    public class LogicTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
